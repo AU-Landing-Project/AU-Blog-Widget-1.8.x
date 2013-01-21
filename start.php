2f2e7f3a3fe8300709e038e918e54e33e4963489
Matt Beckett <beck24@gmail.com>
2013-01-21 12:41:32 -0700

    removes 'all' context keyword

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 
 function au_blog_widget_init() {
   elgg_unregister_widget_type('blog');
-  elgg_register_widget_type('blog', elgg_echo('blog'), elgg_echo('blog:widget:description'), 'all,groups', TRUE);
+  elgg_register_widget_type('blog', elgg_echo('blog'), elgg_echo('blog:widget:description'), 'profile,dashboard,index,groups', TRUE);
 }
 
 elgg_register_event_handler('init', 'system', 'au_blog_widget_init');
